0f36fcb933036fddd0d088cff4a08b906a6316d5
rakeshcooper <rakeshmurali097@gmail.com>
2025-04-03 19:02:56 +0530

    new update

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ let arrayName = document.querySelector(".name");
 let search = document.querySelector(".search-bar")
 aArray = ["Rakesh","Cooper","Stephen","Hawking","Raghav","Cooper"]
 let method = document.querySelector(".method")
-let type;
-let newType
+let type = true;
+let newType = true;
 // arrayName.innerHTML = aArray
 
 method.addEventListener("change", (e) =>{
     console.log(e.target.value);
-    type = true
     if(e.target.value == "regular"){
         newType = type
     } else if(e.target.value == "exact"){
@@ -18,36 +17,46 @@ method.addEventListener("change", (e) =>{
 })
 
 search.addEventListener("input",(e)=>{
-    if(newType == false){
-       console.log(newType);
-    } else {
+    
         let findArray = aArray.map((data) => {
         let dat = data.toLowerCase()
         let txtValue = dat.slice(0)
-        if (txtValue.indexOf(search.value) > -1 ){
+        if(newType == true){
+            if (txtValue.indexOf(search.value) > -1 ){
                 console.log(dat);
                 return dat[0].toUpperCase()+dat.slice(1)  
-        }
+            }
+           
+        }  else if(newType == false) {
+                console.log("test");
+                let found = aArray.find((element) => element == search.value);                
+                console.log(found);
+                return found;
+        }   
+        
         })
 
-        let filteredArray = findArray.filter((data) => {
+         let filteredArray = findArray.filter((data) => {
             return data !== undefined
-        })
-        console.log(findArray);
-        if(filteredArray.length > 0){
-            if(e.target.value.length <= 0) 
-                {
-                    arrayName.innerHTML ="" 
+            })
+            console.log(findArray);
+            if(filteredArray.length > 0){
+                if(e.target.value.length <= 0) 
+                    {
+                        arrayName.innerHTML = "" 
+                    }
+                else{
+                    arrayName.innerHTML = filteredArray
                 }
-            else{
-                arrayName.innerHTML = filteredArray
+            } else{
+                arrayName.innerHTML = "Not Found"
             }
-        } else{
-            arrayName.innerHTML = "Not Found"
-        }
+
         
-    }   
+           
 })
+
+
 
 
 
